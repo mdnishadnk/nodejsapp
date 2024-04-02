f25612427304fa8ab927168b3ba733b41b1ed52c
Nishad <er.nishadnk@gmail.com>
2024-04-03 01:28:39 +0530

    index.js 04

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node world03!\n'
+  const msg = 'Hello Node world04!\n'
   res.end(msg);
 });
 
